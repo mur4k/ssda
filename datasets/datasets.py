9c92d9545231c9c9fa9462cc721aa0f65539eae7
Tymur Mirlas <mirlas.tv@gmail.com>
2020-07-10 12:08:54 +0200

    semantic segmentation dataset

--- datasets/datasets.py
+++ datasets/datasets.py
@@ -62,30 +62,3 @@ class SegmentationDataSet(Dataset):
 			'gt': transforms.ToTensor()(gt),
 			'name': name}
 
-
-if __name__ == '__main__':
-	print("Compiling the dataset")
-	dataset = SegmentationDataSet(root='/nfs/students/mirlas/data', image_list_name='gta5_images.txt', label_list_name='gta5_labels.txt', size=None, mean=(0, 0, 0), std=(1, 1, 1), label2train=None)
-	print("Compiling the dataloader")
-	dataloader = DataLoader(dataset, batch_size=1)
-	print("Length of the DataLoader:", len(dataloader))
-	mean = 0.
-	meansq = 0.
-	std = 0.
-	nb_samples = 0.
-	for i, data in enumerate(dataloader):
-		image = data['image']
-		gt = data['gt']
-		name = data['name']
-		image = image.view(image.size(0), image.size(1), -1)
-		mean += image.mean(2).sum(0)
-		meansq += (image**2).mean(2).sum(0)
-		nb_samples += image.size(0)
-		if i == 0:
-			print(image.shape)
-			print(gt.shape)
-			print(mean, meansq, nb_samples)
-	print(nb_samples)
-	mean = mean / nb_samples
-	print('mean:', mean)
-	print('std:', std)

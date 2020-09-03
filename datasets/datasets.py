9f17c84f637d863fa218a3b0aacbb1dea1846d56
Tymur Mirlas <mirlas.tv@gmail.com>
2020-09-03 12:40:02 +0200

    rotation prediction task

--- datasets/datasets.py
+++ datasets/datasets.py
@@ -17,13 +17,7 @@ class SegmentationDataSet(Dataset):
         self.label2train = label2train
         self.mean = mean
         self.std = std
-        self.image_preprocess_transform = transforms.Compose([
-            transforms.Resize(self.size, Image.BILINEAR), 
-            transforms.ToTensor(), 
-            transforms.Normalize(mean=self.mean, std=self.std)])
-        self.label_preprocess_transform = transforms.Compose([
-            transforms.Resize(self.size, Image.NEAREST),
-            transforms.Lambda(lambda gt: torch.tensor(np.array(gt, dtype=np.int64)))])
+        self.compose_transforms()
         # read lists of files
         self.image_list_path = os.path.join(root, self.image_list_name)
         self.label_list_path = os.path.join(root, self.label_list_name)
@@ -35,18 +29,73 @@ class SegmentationDataSet(Dataset):
         self.labels_paths = [p for p in self.labels_paths if os.path.isfile(p)]
         self.files = list(zip(self.images_paths, self.labels_paths))
 
+    def compose_transforms(self):
+        self.image_preprocess_transform = transforms.Compose([
+            transforms.Resize(self.size, Image.BILINEAR), 
+            transforms.ToTensor(), 
+            transforms.Normalize(mean=self.mean, std=self.std)])
+        self.label_preprocess_transform = transforms.Compose([
+            transforms.Resize(self.size, Image.NEAREST),
+            transforms.Lambda(lambda gt: torch.tensor(np.array(gt, dtype=np.int64)))])
+
+    def read_data(self, index):
+        image_path, label_path = self.files[index]
+        image = Image.open(image_path).convert('RGB')
+        label = Image.open(label_path)
+        name = os.path.basename(image_path)
+        return image, label, name
+
     def __len__(self):
         return len(self.files)
 
     def __getitem__(self, index):
-        image_path, label_path = self.files[index]
-
-        image = Image.open(image_path).convert('RGB')
-        label = Image.open(label_path)
-        name = os.path.basename(image_path)
+        image, label, name = self.read_data(index)
 
         data = {'image': self.image_preprocess_transform(image), 
             'gt': self.label_preprocess_transform(label),
+            'name': name}
+
+        # re-assign labels to match the unified format
+        if self.label2train is not None:
+            for k, v in self.label2train.items():
+                data['gt'][data['gt'] == k] = v
+
+        return data
+
+
+class RotationDataSet(SegmentationDataSet):
+    def __init__(self, root, image_list_name, label_list_name, size, size_crop, 
+        mean=(0, 0, 0), std=(1, 1, 1), label2train=None):
+        self.rot_classes = 3
+        self.size_crop = size_crop
+        super().__init__(root, image_list_name, label_list_name, size, mean, std, label2train)
+
+    def compose_transforms(self):
+        self.image_preprocess_transform = transforms.Compose([
+            transforms.Resize(self.size, Image.BILINEAR)])
+        self.image_aux_preprocess_transform = transforms.Compose([
+            transforms.RandomCrop(self.size_crop)])
+        self.aux_transform = transforms.functional.rotate
+        self.normalize_transform = transforms.Compose([
+            transforms.ToTensor(), 
+            transforms.Normalize(mean=self.mean, std=self.std)])
+        self.label_preprocess_transform = transforms.Compose([
+            transforms.Resize(self.size, Image.NEAREST),
+            transforms.Lambda(lambda gt: torch.tensor(np.array(gt, dtype=np.int64)))])
+
+    def __getitem__(self, index):
+        image, label, name = self.read_data(index)
+
+        image = self.image_preprocess_transform(image)
+        aux_image = self.image_aux_preprocess_transform(image)
+
+        aux_label = torch.randint(self.rot_classes+1, size=(1,)).squeeze()  # added 1 for class 0: unrotated
+        aux_image = self.aux_transform(img=aux_image, angle=aux_label*90)
+
+        data = {'image': self.normalize_transform(image), 
+            'aux_image': self.normalize_transform(aux_image),
+            'gt': self.label_preprocess_transform(label),
+            'aux_gt': aux_label,
             'name': name}
 
         # re-assign labels to match the unified format
---
+++ models/classifier.py
@@ -0,0 +1,59 @@
+import torch.nn as nn
+import torch.nn.functional as F
+
+class FCClassifier(nn.Module):
+    def __init__(self, input_dim, num_classes, avg_pool=True, ndf=64):
+        super(FCDiscriminator, self).__init__()
+        self.conv1 = nn.Conv2d(input_dim, ndf, kernel_size=4, stride=2, padding=1)
+        self.conv2 = nn.Conv2d(ndf, ndf*2, kernel_size=4, stride=2, padding=1)
+        self.conv3 = nn.Conv2d(ndf*2, ndf*4, kernel_size=4, stride=2, padding=1)
+        self.conv4 = nn.Conv2d(ndf*4, ndf*8, kernel_size=4, stride=2, padding=1)
+        self.classifier = nn.Conv2d(ndf*8, num_classes, kernel_size=4, stride=1, padding=1)
+        self.leaky_relu = nn.LeakyReLU(negative_slope=0.2, inplace=True)
+        self.avg_pool = avg_pool
+        if self.avg_pool:
+            self.reduce_dim = nn.AdaptiveAvgPool2d(1)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.leaky_relu(x)
+        x = self.conv2(x)
+        x = self.leaky_relu(x)
+        x = self.conv3(x)
+        x = self.leaky_relu(x)
+        x = self.conv4(x)
+        x = self.leaky_relu(x)
+        x = self.classifier(x)
+        if self.avg_pool:
+            x = self.reduce_dim(x)
+        return x
+
+
+class FCClassifierBatchNorm(nn.Module):
+    def __init__(self, input_dim, num_classes, ndf=64):
+        super(Discriminator, self).__init__()
+        self.main = nn.Sequential(
+            # input is (input_dim) x w x h
+            nn.Conv2d(input_dim, ndf, 4, 2, 1, bias=False),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. (ndf) x w/2 x h/2
+            nn.Conv2d(ndf, ndf*2, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ndf*2),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. (ndf*2) x w/4 x h/4
+            nn.Conv2d(ndf*2, ndf*4, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ndf*4),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. (ndf*4) x w/8 x h/8
+            nn.Conv2d(ndf*4, ndf*8, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ndf*8),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. (ndf*8) x w/16 x h/16
+            nn.Conv2d(ndf*8, num_classes, 4, 1, 1, bias=False),
+        )
+
+    def forward(self, x):
+        x = self.main(x)
+        if self.avg_pool:
+            x = self.reduce_dim(x)
+        return x
